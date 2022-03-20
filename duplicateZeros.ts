703cc59c31b686eb5b9bb398ae298ac543af3065
Ocholla-T <tonnyocholla8@gmail.com>
2022-03-20 17:56:05 +0300

    duplicate zeros

---
+++ duplicateZeros.ts
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+function duplicateZeros(arr: number[]): void {
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index] === 0) {
+      arr.splice(index + 1, 0, 0);
+      arr.pop();
+      index++;
+    }
+  }
+}
+
+console.log(duplicateZeros([1, 2, 3]));
